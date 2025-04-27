003d57bbe00857c7a309b4c01a7258dbec536606
srinibas khuntia <srinibas.k@pillir.io>
2025-04-27 21:44:32 +0530

    implemented broadcasting message

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,58 @@ const wss = new WebSocketServer({port:5000});
 
 let userCount = 0;
 
-let webSockets:WebSocket[] = []
+// let webSockets:WebSocket[] = []
+
+interface User {
+    socket:WebSocket,
+    roomId:string
+}
+
+
+
+let users:User[] = []
 
 wss.on("connection",(ws)=>{
-    webSockets.push(ws)
-    userCount+=1;
-    console.log("userCount ",userCount)
+    // webSockets.push(ws)
+    userCount++;
     ws.on("error",(err)=>{
         console.log("err")
     })
 
-    ws.on("message",(msg)=>{
-        setTimeout(()=>{
-            // ws.send("Send From server "+msg.toString())
-            webSockets.forEach(socket=>{
-                setTimeout(()=>{
-                    socket.send("FROM SERVER "+msg.toString().toUpperCase())
-                },1000)
+    ws.on("message",(payload:string)=>{
+        const parseData = JSON.parse(payload);
+        if(parseData.type === "join"){
+            const roomId = parseData.payload.roomId;
+            users.push({
+                socket:ws,
+                roomId
             })
-        },1000)
+        }
+        else{
+            //chat
+            //check if sokcet exist or not
+            const user = users.find(user => user.socket === ws)
+
+            if(!user){
+                return;
+            }
+
+            //broadcast the message to all the scokets which have same roo id
+            const scoketUsers = users.filter(u => u.roomId === user.roomId && u.socket!=user.socket);
+            scoketUsers.forEach(user =>{
+                user.socket.send(JSON.stringify({
+                    type:"chat",
+                    payload:{
+                        message :parseData.payload.message
+                    }
+                }))
+            })
+
+        }
     })
 
     ws.on("close",()=>{
-        webSockets.filter(sock => sock !=ws)
+        users = users.filter(user=>user.socket !== ws);
         userCount--;
         console.log("userCOunt",userCount)
     })
