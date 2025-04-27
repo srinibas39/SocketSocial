4c5d40f2d6a9d06a2543944406c3fc43de861973
srinibas khuntia <srinibas.k@pillir.io>
2025-04-27 18:14:05 +0530

    implemented the broadcasting logic

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ const wss = new WebSocketServer({port:5000});
 
 let userCount = 0;
 
+let webSockets:WebSocket[] = []
+
 wss.on("connection",(ws)=>{
+    webSockets.push(ws)
     userCount+=1;
     console.log("userCount ",userCount)
     ws.on("error",(err)=>{
@@ -13,8 +16,19 @@ wss.on("connection",(ws)=>{
 
     ws.on("message",(msg)=>{
         setTimeout(()=>{
-            ws.send("Send From server "+msg.toString())
+            // ws.send("Send From server "+msg.toString())
+            webSockets.forEach(socket=>{
+                setTimeout(()=>{
+                    socket.send("FROM SERVER "+msg.toString().toUpperCase())
+                },1000)
+            })
         },1000)
+    })
+
+    ws.on("close",()=>{
+        webSockets.filter(sock => sock !=ws)
+        userCount--;
+        console.log("userCOunt",userCount)
     })
 
 })
